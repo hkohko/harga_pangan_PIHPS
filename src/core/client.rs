6012e63deca5855964c64c11cc739851cbddfe4b
heko <91366057+hkohko@users.noreply.github.com>
2023-11-17 13:24:34 +0700

    get month name array

--- src/core/client.rs
+++ src/core/client.rs
@@ -11,22 +11,41 @@ use std::io::BufReader;
 
 pub fn client_main(date: &HashMap<&str, u32>) {
     if let Err(e) = process_client(date) {
-        println!("{e}");
+        panic!("{e}");
     }
 }
 fn process_client(date: &HashMap<&str, u32>) -> Result<()> {
-    let get_choices = data_choices(date);
-    let url = get_url()?;
-    let get_header = get_header()?;
-    let header = build_header(&get_header)?;
-    let client = build_client(header)?;
-    let make_req = process_req(&client, &url)?;
+
+    let raw_url = get_url()?;
+    let url = data_choices(date, &raw_url);
+    // let get_header = get_header()?;
+    // let header = build_header(&get_header)?;
+    // let client = build_client(header)?;
+    // let make_req = process_req(&client, &url)?;
     // let save_response = save_resp(make_req)?;
     Ok(())
 }
-fn data_choices(date: &HashMap<&str, u32>) {
+fn data_choices(date: &HashMap<&str, u32>, raw_url: &String) -> Result<()> {
     let commodity: &str;
-    let date: &str;
+    let mut file_month_path = ProjPaths::res_path()?;
+    file_month_path.push("month.json");
+
+    let retval_month = || -> Result<serde_json::Value> {
+        let mut month_list = String::new();
+        let f_month = fs::File::open(file_month_path)?;
+        let mut reader = BufReader::new(f_month);
+        reader.read_to_string(&mut month_list)?;
+        let month_as_serde_val: serde_json::Value = serde_json::from_str(month_list.as_str()).unwrap();
+        Ok(month_as_serde_val)
+    };
+    let to_month_dict = |m_val: &serde_json::Value| -> Result<serde_json::Value> {
+        let month_obj = m_val.as_object().unwrap();
+        let month_names = month_obj.get("month_date").unwrap();
+        Ok(month_names.clone())
+    };
+    let month_as_serde_val = retval_month()?;
+    let month_dict = to_month_dict(&month_as_serde_val);
+    Ok(())
 }
 fn get_url() -> Result<String> {
     let filename = "url.txt";
