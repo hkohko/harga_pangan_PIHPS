be093be6f3cf2163c161a83be018903622d0d87c
heko <91366057+hkohko@users.noreply.github.com>
2023-11-17 07:39:54 +0700

    useless commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+#![allow(dead_code, unused_variables)]
+use anyhow::Result;
 use get_prices::core::client::client_main;
-use get_prices::core::parse_date::parser_main;
+use get_prices::core::parse_date::parser;
+use get_prices::input::handle_input::sanitize;
 fn main() {
-    let _ = parser_main();
+    let _ = main_2();
+}
+fn main_2() -> Result<()> {
+    let input = sanitize()?;
+    let date = parser(input.as_str())?;
+    let client = client_main(&date);
+    println!("{:?}", date);
+    Ok(())
 }
--- src/optional/opt.rs
+++ src/optional/opt.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, unused_imports, unused_variable)]
+#![allow(dead_code, unused_imports, unused_variables)]
 use std::path::PathBuf;
 use anyhow::Result;
 use std::io::BufWriter;
