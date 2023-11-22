6158e2c26f8a3431925888a3d4b3ac4ca94f794c
heko <91366057+hkohko@users.noreply.github.com>
2023-11-22 13:14:57 +0700

    parsed result.json

---
+++ src/core/parse_json.rs
@@ -0,0 +1,25 @@
+use serde_json;
+use std::io::{self, BufReader, prelude::*};
+use std::fs;
+use anyhow::Result;
+use crate::core::path::ProjPaths;
+use crate::ResultObjects;
+
+pub fn parse_json_main() {
+    if let Err(e) = parse(){
+        panic!("{e}");
+    }
+}
+fn parse() -> Result<()>{
+    let vec: Vec<ResultObjects> = {
+        let mut s = String::new();
+        let mut filepath = ProjPaths::res_path()?;
+        let _ = &filepath.push("result.json");
+        let f = fs::File::open(filepath)?;
+        let mut reader = BufReader::new(f);
+        reader.read_to_string(&mut s).expect("reader failed.");
+        serde_json::from_str(&s).expect("serde_json fail")
+    };
+    dbg!(vec);
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use serde::Deserialize;
+
 pub mod core {
     pub mod client;
     pub mod parse_date;
     pub mod parse_url;
+    pub mod parse_json;
     pub mod path;
 }
 pub mod input {
@@ -12,4 +15,22 @@ pub mod optional {
 }
 pub mod data {
     pub mod constants;
+}
+#[derive(Deserialize, Debug)]
+pub struct ResultObjects {
+    ProvID: i32,
+    Provinsi: String,
+    Tanggal: String,
+    Komoditas: String,
+    Nilai: f32,
+    NilaiDiff: String,
+    SemuaProvinsi: f32,
+    Kelompok: i32,
+    stdDev: f32,
+    Percentage: f32,
+    SemuaPercentage: f32,
+    stdDevPercentage: Option<String>,
+    TanggalLast: String,
+    TanggalInflasi: String,
+    show: bool,
 }
